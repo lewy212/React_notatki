4d757b6709c06defd3e7b5e885f1ffc82d0fba47
lewy212 <huak@wp.pl>
2024-01-21 14:48:43 +0100

    notatki part2

--- src/Note.js
+++ src/Note.js
@@ -1,30 +1,122 @@
 import React from "react";
 import PropTypes from "prop-types";
+import * as Icon from "react-bootstrap-icons"
 
 const Note = (props)=>{
     return(
         <tr>
-            <td>{props.title}</td>
-            <td>{props.category}</td>
-            <td>{props.content}</td>
-            <td>{props.date.getDate()}.{props.date.getMonth()+1}.{props.date.getFullYear()}</td>
+            <td style={{verticalAlign: "middle"}}>{props.title}</td>
+            <td className="justifyView" style={{borderBottom: "none"}}>{props.category}
+                {
+                    props.category ==="To do" ?(
+                        <Icon.ListCheck size={66}/>
+                    ) : (
+                        <Icon.FileText size={66} />
+                    )}
+            </td>
+            <td style={{verticalAlign: "middle"}}>{props.content}</td>
+            {
+                props.status === undefined ? (
+                    <td>
+                        <div className="justifyRow">
+                            <div className="justifyColumn">
+                                <div>
+                                    <Icon.Bell size={32} color ="green" className="item"/>
+                                    <i>Detail</i>
+                                </div>
+                                <div style={{display: "block"}}>
+                                    <Icon.Bell size={32} color ="#999900" className="item"/>
+                                    <i>Remind</i>
+                                </div>
+                            </div>
+                            <div className="justifyColumn">
+                                <div>
+                                    <Icon.Pencil size={30} color ="blue" className="item"/>
+                                    <i>Edit</i>
+                                </div>
+                                <div>
+                                    <Icon.Trash size={30} color ="black" className="item"/>
+                                    <i>Delete</i>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                ) : (
+                    <td>
+                        {
+                            props.status ? (
+                                <div className="justifyRow">
+                                    <div className ="justifyColumn">
+                                        <div style ={{display: "block", verticalAlign: "middle"}}>
+                                            <Icon.XCircle size={32} color="red" className="item" />
+                                            <i>Undone</i>
+                                        </div>
+                                        <div>
+                                            <Icon.Bell size={30} color ="green" className="item"/>
+                                            <i>Detail</i>
+                                        </div>
+                                    </div>
+                                    <div className="justifyColumn">
+                                        <div style={{display: "block"}}>
+                                            <Icon.Bell size={32} color ="#999900" className="item"/>
+                                            <i>Remind</i>
+                                        </div>
+                                        <div style={{display: "block"}}>
+                                            <Icon.Pencil size={32} color ="blue" className="item"/>
+                                            <i>Edit</i>
+                                        </div>
+                                    </div>
+                                    <div className="justifyColumn">
+                                        <div>
+                                            <Icon.Trash size={30} color ="black" className="item"/>
+                                            <i>Delete</i>
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className="justifyRow">
+                                    <div className="justifyColumn">
+                                        <div style={{display: "block"}}>
+                                            <Icon.CheckCircle size={32} color ="green" className="item"/>
+                                            <i>Done</i>
+                                        </div>
+                                        <div>
+                                            <Icon.List size={30} color ="green" className="item"/>
+                                            <i>Detail</i>
+                                        </div>
+                                    </div>
+                                    <div className="justifyColumn">
+                                        <div style={{display: "block"}}>
+                                            <Icon.Bell size={32} color ="#999900" className="item"/>
+                                            <i>Remind</i>
+                                        </div>
+                                        <div>
+                                            <Icon.Pencil size={30} color ="blue" className="item"/>
+                                            <i>Edit</i>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <Icon.Trash size={30} color ="black" className="item"/>
+                                        <i>Delete</i>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </td>
+                )
+            }
         </tr>
-    )
-}
+    );
+};
 Note.propTypes={
     title(props,propName){
         if(props[propName].length<3){
             return new Error(propName +" was to short")
         }
     },
-    category: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    date: PropTypes.instanceOf(Date).isRequired
+    category: PropTypes.string,
+    content: PropTypes.string
+
 };
-Note.defaultProps={
-    title:"NoteTitle",
-    category:"NoteCategory",
-    content:"NoteContent",
-    date:new Date()
-}
+
 export default Note;
--- src/Notes.js
+++ src/Notes.js
@@ -1,58 +1,169 @@
-import React from 'react';
-import {Table} from 'react-bootstrap'
+import React, {Component} from 'react';
+import {Button, Table} from 'react-bootstrap'
 import Note from "./Note";
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { confirmAlert } from 'react-confirm-alert';
 
 
-var noteList =[
-    {
-        title: "Go to the gym",
-        category:"Hobby",
-        content:"Leg day",
-        date: new Date("2020-06-06")
-    },
-    {
-        title: "Go to the gym",
-        category:"Hobby",
-        content:"Chest day",
-        date: new Date("2020-07-07")
+class Notes extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            noteList :[
+                {
+                    title:"Meeting",
+                    category: "Work",
+                    content: "Meet with John in restaurant Bistro",
+                    date: '',
+                    status: undefined,
+                    time: ''
+                },
+                {
+                    title:"Test score",
+                    category: "Study",
+                    content: "25/30 points on practical part",
+                    date: '',
+                    status: undefined,
+                    time: ''
+                },
+                {
+                    title: "Go to the gym",
+                    category:"Hobby",
+                    content:"Leg day",
+                    date: '',
+                    status: false,
+                    time: ''
+                },
+                {
+                    title: "Go to the gym",
+                    category:"Hobby",
+                    content:"Chest day",
+                    date: new Date("2020-07-07")
+                }
+            ],
+            title: '',
+            category: '',
+            content: '',
+            date: undefined,
+            time: undefined,
+        }
     }
-]
-function Notes(){
-    const header = <h3>List of  notes</h3>;
-    return (
-        <div>
-            {header}
-            <Table striped bordered>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Category</th>
-                    <th>Content</th>
-                    <th>Date</th>
-                </tr>
-                <Note
-                    title="Go shopping"
-                    category="To do"
-                    content = "Buy some vegetables"
-                    date={new Date("2020-12-12")}
-                />
-                {noteList.map((note,key)=>{
-                    return(
-                        <Note
-                            key={key}
-                            title={note.title}
-                            category={note.category}
-                            content={note.content}
-                            date={note.date}
+    onChange(e){
+        var name = e.target.id;
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+    onClick(){
+        confirmAlert({
+            customUI: ({onClose})=>{
+                return(
+                    <div>
+                        <h1>Add content to note</h1>
+                        <p><textarea cols="50" rows="10" id="content" defaultValue={this.state.content} onChange={(e)=> this.onChange(e)}></textarea> </p>
+                        <Button style={{float:"right"}} variant="danger" onClick={onClose}>Close window</Button>
+                    </div>
+                );
+            }
+        });
+    }
+    addNote(){
+        this.setState(state=>{
+            var notes = state.noteList;
+            var date = state.date === undefined ? "": new Date(state.date);
+            var time = state.time === undefined ? "": state.time;
+            if(state.category ==="To do"){
+                notes.push({
+                    title: state.title,
+                    category: state.category,
+                    content: state.content,
+                    date: date,
+                    time: time,
+                    status: false
+                })
+            } else {
+                notes.push({
+                    title: state.title,
+                    category: state.category,
+                    content: state.content,
+                    date: date,
+                    time: time,
+                    status: undefined
+                })
+            }
+            return{noteList : notes}
+        })
+    }
+    filter(content) {
+        return content.length > 25 ? content.substring(0,25) +"..." : content;
+    }
+    render() {
+        return (
+            <div>
+                <h3>List of  notes</h3>;
+                <Table striped bordered>
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Content</th>
+                        <th>Action</th>
+                    </tr>
+                    <Note
+                        title="Go shopping"
+                        category="To do"
+                        content = "Buy some vegetables"
+                        date={new Date("2020-12-12")}
+                    />
+                    {this.state.noteList.map((note,key)=>{
+                        return(
+                            <Note
+                                key={key}
+                                title={note.title}
+                                category={note.category}
+                                content={this.filter(note.content)}
+                                status={note.status}
                             />
-                    )
-                })}
-                <Note />
-                </thead>
-                <tbody>
-                </tbody>
-            </Table>
-        </div>
-    );
+                        )
+                    })}
+                    <Note />
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </Table>
+                <Table striped bordered>
+                    <tbody>
+                    <tr>
+                        <td colSpan="5" style={{textAlign: "center"}}><i><b>Add new note</b></i></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" placeholder="Title of note" id="title" onChange={(e)=> this.onChange(e)} /> </td>
+                        <td><input type="text" list="categoryList" id="category" placeholder="Category of note" onChange={(e)=> this.onChange(e)} />
+                        <datalist id="categoryList">
+                            <option>To do</option>
+                            <option>Hobby</option>
+                            <option>Work</option>
+                            <option>Study</option>
+                            <option>Gym</option>
+                            <option>Favorites</option>
+                        </datalist>
+                        </td>
+                        <td>
+                            {
+                                this.state.content!==''?
+                                    <Button variant="primary" onClick={()=>this.onClick()}>Edit content</Button>:
+                                    <Button variant="success" onClick={()=>this.onClick()}>Add content</Button>
+                            }
+                        </td>
+                        <td><input type="date" id="date" onChange={(e)=>this.onChange(e)} /><input type="time" id="time" onChange={(e)=>this.onChange(e)}/></td>
+                        <td><Button variant="secondary" onClick={()=>this.addNote()}>Add note</Button> </td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+
+
 }
 export default Notes;
